77bf12f1683e6aedd4786b837a8542e3877ab9d7
artbigtema <belfegor732@gmail.com>
2016-03-31 19:34:51 +0400

    added libraries: butterknife, cardview, recycleview. added item of listview.

--- app/src/main/java/av/tesktask/yamobilizationapp/MainActivity.java
+++ app/src/main/java/av/tesktask/yamobilizationapp/MainActivity.java
@@ -1,13 +1,21 @@
 package av.tesktask.yamobilizationapp;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+        import android.support.v7.app.AppCompatActivity;
+        import android.support.v7.widget.RecyclerView;
+        import android.os.Bundle;
+
+        import butterknife.Bind;
+        import butterknife.ButterKnife;
+
 
 public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.rv_artists_list)
+    RecyclerView artistList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
